781a8577d46f414f8f3719284630a32cb8c4f1cd
solo5star <solo_5@naver.com>
2021-07-30 19:52:14 +0900

    Set encoding null to preserve byte array (byte array will modified by default, since encoding is utf8 !!! this breaks media files such as png, jpeg)

--- bootstrap.js
+++ bootstrap.js
@@ -25,7 +25,8 @@ module.exports = app => {
             resolveWithFullResponse: true,
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
-            }
+            },
+            encoding: null,
         })
         .then(originResponse => {            
             responseBuilder
